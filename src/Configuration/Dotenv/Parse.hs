fb84a9c848c75eea57ce7127e8499b69b5753eae
Justin Leitgeb <justin@stackbuilders.com>
2015-02-08 20:14:03 -0500

    Improve parsing error messages

--- src/Configuration/Dotenv/Parse.hs
+++ src/Configuration/Dotenv/Parse.hs
@@ -1,6 +1,6 @@
 module Configuration.Dotenv.Parse (configParser) where
 
-import Text.Parsec ((<|>), anyChar, char, many, manyTill, try)
+import Text.Parsec ((<|>), (<?>), anyChar, char, many, manyTill, try)
 import Text.Parsec.Combinator (eof)
 import Text.Parsec.String (Parser)
 import Text.ParserCombinators.Parsec.Char
@@ -9,6 +9,7 @@ import Text.ParserCombinators.Parsec.Char
 import Control.Applicative ((<*), (*>), (<$>))
 import Data.Maybe (catMaybes)
 import Control.Monad (liftM2)
+import Data.List (intercalate)
 
 -- | Returns a parser for a Dotenv configuration file.  Accepts key
 -- and value arguments separated by "=".  Comments are allowed on
@@ -21,47 +22,61 @@ envLine :: Parser (Maybe (String, String))
 envLine = (comment <|> blankLine) *> return Nothing <|> Just <$> optionLine
 
 blankLine :: Parser String
-blankLine = many verticalSpace <* newline
+blankLine = many verticalSpace <* newline <?> "blank line"
 
 optionLine :: Parser (String, String)
 optionLine = liftM2 (,)
-  (many verticalSpace *> variableName <* keywordArgSeparator)
-  argumentParser
+  (many verticalSpace *> variableName <* variableValueSeparator)
+  value
 
 -- | Variables must start with a letter or underscore, and may contain
 -- letters, digits or '_' character after the first character.
 variableName :: Parser String
 variableName =
-  liftM2 (:) (letter <|> char '_') (many (letter <|> char '_' <|> digit))
+  liftM2 (:)
 
-argumentParser :: Parser String
-argumentParser = quotedArgument <|> unquotedArgument
+  (letter <|> char '_')
 
-quotedArgument :: Parser String
-quotedArgument = quotedWith '\'' <|> quotedWith '\"'
+  (many
+   (letter <|> char '_' <|> digit <?>
+    intercalate " "
+    [ "valid non-leading shell variable character (alphanumeric, "
+    , "digit or underscore)" ]))
 
-unquotedArgument :: Parser String
-unquotedArgument =
+  <?> intercalate " " [ "shell variable name (letter or underscore followed"
+                      , "by alphanumeric characters or underscores)" ]
+
+value :: Parser String
+value = quotedValue <|> unquotedValue <?> "variable value"
+
+quotedValue :: Parser String
+quotedValue = quotedWith '\'' <|> quotedWith '\"'
+              <?> "variable value surrounded with single or double quotes"
+
+unquotedValue :: Parser String
+unquotedValue =
   manyTill anyChar (comment <|> many verticalSpace <* endOfLineOrInput)
 
 -- | Based on a commented-string parser in:
 -- http://hub.darcs.net/navilan/XMonadTasks/raw/Data/Config/Lexer.hs
 quotedWith :: Char -> Parser String
-quotedWith c = char c *> many chr <* char c
+quotedWith c = char c *> many chr <* (char c <?> "closing quote character")
 
   where chr = esc <|> noneOf [c]
-        esc = escape *> char c
+        esc = escape *> char c <?> "escape character"
 
 comment :: Parser String
 comment = try (many verticalSpace *> char '#')
           *> manyTill anyChar endOfLineOrInput
+          <?> "comment"
 
 endOfLineOrInput :: Parser ()
 endOfLineOrInput = newline *> return () <|> eof
 
-keywordArgSeparator :: Parser ()
-keywordArgSeparator =
-  many verticalSpace *> char '=' *> many verticalSpace *> return ()
+variableValueSeparator :: Parser ()
+variableValueSeparator =
+  many verticalSpace *> (char '=' <?> "variable-value separator character (=)")
+  *> many verticalSpace *> return ()
 
 escape :: Parser Char
 escape = char '\\'
