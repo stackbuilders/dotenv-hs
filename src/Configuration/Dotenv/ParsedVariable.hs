c818e24fe7f8f2116df0af3726592bf188fc1044
Habib Alamin <ha.alamin@gmail.com>
2017-02-23 19:25:55 +0000

    Interpolate references to previous variables / environment.

--- src/Configuration/Dotenv/ParsedVariable.hs
+++ src/Configuration/Dotenv/ParsedVariable.hs
@@ -2,7 +2,12 @@ module Configuration.Dotenv.ParsedVariable (ParsedVariable(..),
                                             VName,
                                             VValue(..),
                                             VContents,
-                                            VFragment(..)) where
+                                            VFragment(..),
+                                            interpolateParsedVariables) where
+
+import Control.Monad (foldM)
+import Control.Applicative ((<|>))
+import System.Environment (lookupEnv)
 
 data ParsedVariable
   = ParsedVariable VName VValue
@@ -19,3 +24,23 @@ type VContents = [VFragment]
 data VFragment
   = VInterpolation String
   | VLiteral String
+
+interpolateParsedVariables :: [ParsedVariable] -> IO [(String, String)]
+interpolateParsedVariables = foldM addInterpolated []
+
+addInterpolated :: [(String, String)] -> ParsedVariable -> IO [(String, String)]
+addInterpolated previous (ParsedVariable name value) = (: previous) <$> ((,) name <$> interpolate previous value)
+
+interpolate :: [(String, String)] -> VValue -> IO String
+interpolate _        (SingleQuoted contents) = return contents
+interpolate previous (DoubleQuoted contents) = interpolateContents previous contents
+interpolate previous (Unquoted     contents) = interpolateContents previous contents
+
+interpolateContents :: [(String, String)] -> VContents -> IO String
+interpolateContents previous contents = concat <$> mapM (interpolateFragment previous) contents
+
+interpolateFragment :: [(String, String)] -> VFragment -> IO String
+interpolateFragment _        (VLiteral       value  ) = return value
+interpolateFragment previous (VInterpolation varname) = fromPreviousOrEnv >>= maybe (return "") return
+  where
+    fromPreviousOrEnv = (lookup varname previous <|>) <$> lookupEnv varname
