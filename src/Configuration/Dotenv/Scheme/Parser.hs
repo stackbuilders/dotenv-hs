8f36cf7258721b6d67a5f7dcbdfba35a1c5ac7c6
CristhianMotoche <cristhian.motoche@gmail.com>
2017-10-23 09:00:46 -0500

    Create areParseable function

--- src/Configuration/Dotenv/Scheme/Parser.hs
+++ src/Configuration/Dotenv/Scheme/Parser.hs
@@ -8,7 +8,17 @@ import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
+import Configuration.Dotenv.Scheme.Helpers
 import Configuration.Dotenv.Scheme.Types
+
+areParseable
+  :: Config
+  -> [(String, String)]
+  -> Bool
+areParseable config envs =
+  let envsWithConf = mapMatchEnvWithConf config envs
+      canBeParsed (v, t)= v `isParseableAs` t
+   in all canBeParsed envsWithConf
 
 isParseableAs
   :: String -- ^ Value of the env variable
