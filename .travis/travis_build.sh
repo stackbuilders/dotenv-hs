170457eb2978afc8f00efa33c93c1f11fa83a2e1
CristhianMotoche <cristhian.motoche@gmail.com>
2020-04-01 15:18:36 -0500

    Refactor: Use stages with parallel jobs

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -3,9 +3,10 @@
 if [ "$TRAVIS_OS_NAME" = "osx" ]
 then
   cabal install
+  tar -C dist/build/dotenv -cvf dotenv-$TRAVIS_OS_NAME-x86_64.tar.gz dotenv
+  mv dotenv-$TRAVIS_OS_NAME-x86_64.tar.gz ~/$TRAVIS_BUILD_NUMBER/
 else
   cabal install -f 'static'
+  tar -C dist/build/dotenv -cvf dotenv-$TRAVIS_OS_NAME-x86_64-static.tar.gz dotenv
+  mv dotenv-$TRAVIS_OS_NAME-x86_64-static.tar.gz ~/$TRAVIS_BUILD_NUMBER/
 fi
-
-tar -C dist/build/dotenv -cvf dotenv-$TRAVIS_OS_NAME-x86_64-static.tar.gz dotenv
-mv dotenv-$TRAVIS_OS_NAME-x86_64-static.tar.gz ~/$TRAVIS_BUILD_NUMBER/
