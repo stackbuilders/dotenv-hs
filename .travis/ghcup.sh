79775fdc6bb90a8de1e94f8247ffe75b560a3c6c
Stefani Castellanos <scct95@hotmail.com>
2021-06-04 11:36:29 -0500

    Remove workflow on Travis

--- .travis/ghcup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-( mkdir -p ~/.ghcup/bin && curl https://gitlab.haskell.org/haskell/ghcup/raw/master/ghcup > ~/.ghcup/bin/ghcup && chmod +x ~/.ghcup/bin/ghcup) && echo "success"
-ghcup install $GHC_VER
-ghcup install-cabal $CABAL_VER
-ghcup set $GHC_VER
-cabal update
