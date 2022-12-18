31bc442e9334f7135438c085a09f06ac9eb7d600
CristhianMotoche <cristhian.motoche@gmail.com>
2022-12-17 20:05:09 -0500

    modify: prepare everything for review

--- build.static.sh
+++ build.static.sh
@@ -1,6 +1,9 @@
-echo 'STARTING'
+echo 'cabal update'
 cabal update
+echo 'build dependencies'
 cabal build --only-dependencies --enable-static
+echo 'build static executable'
 cabal build --enable-executable-static
+echo 'copy bin'
 cp $(cabal list-bin dotenv) dotenv
-echo 'DONE!'
+echo 'finished'
