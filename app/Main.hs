030fbdada494279999099228a996818709e06cc1
Alexander Goussas <84427521+aloussase@users.noreply.github.com>
2024-10-18 16:36:14 -0500

    Parse configuration file from current directory for additional flags (#197)
    * feature: Parse configuration file from current directory for additional flags
    
    * fix: Use readFile instead of readFile' for GHC 8.10 on ubuntu-latest

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ import           Options.Applicative
 import           Paths_dotenv         (version)
 
 import           Configuration.Dotenv (Config (..), defaultConfig, loadFile)
+import           System.Directory     (doesFileExist)
 import           System.Exit          (exitWith)
 import           System.Process       (system)
 
@@ -26,9 +27,42 @@ data Options = Options
   , args               :: [String]
   } deriving (Show)
 
+data Flags = Flags
+  { flagDotenvFiles        :: [String]
+  , flagDotenvExampleFiles :: [String]
+  , flagOverride           :: Bool
+  , flagVerbose            :: Bool
+  , flagDryRun             :: Bool
+  , flagDuplicates         :: Bool
+  }
+
+defaultFlags :: Flags
+defaultFlags = Flags
+  { flagDotenvFiles = []
+  , flagDotenvExampleFiles = []
+  , flagOverride = False
+  , flagVerbose = False
+  , flagDryRun = False
+  , flagDuplicates = False
+  }
+
+applyFlags :: Options -> Flags -> Options
+applyFlags options flags = Options
+  { dotenvFiles = dotenvFiles options <> flagDotenvFiles flags
+  , dotenvExampleFiles = dotenvExampleFiles options <> flagDotenvExampleFiles flags
+  , override = override options || flagOverride flags
+  , verbose = verbose options || flagVerbose flags
+  , dryRun = dryRun options || flagDryRun flags
+  , duplicates = duplicates options || flagDuplicates flags
+  , program = program options
+  , args = args options
+  }
+
 main :: IO ()
 main = do
-  Options{..} <- execParser opts
+  options <- execParser $ mkOpts config
+  flags <- loadFlagsFromConfig
+  let Options{..} = applyFlags options flags
   let configDotenv =
         Config
           { configExamplePath = dotenvExampleFiles
@@ -46,45 +80,86 @@ main = do
     if configDryRun configDotenv
       then putStrLn "[INFO]: Dry run mode enabled. Not executing the program."
       else system (program ++ concatMap (" " ++) args) >>= exitWith
-       where
-         opts = info (helper <*> versionOption <*> config)
-           ( fullDesc
-          <> progDesc "Runs PROGRAM after loading options from FILE"
-          <> header "dotenv - loads options from dotenv files" )
-         versionOption =
-           infoOption
+
+mkOpts :: Parser a -> ParserInfo a
+mkOpts p = info (helper <*> versionOption <*> p)
+    ( fullDesc
+    <> progDesc "Runs PROGRAM after loading options from FILE"
+    <> header "dotenv - loads options from dotenv files" )
+    where
+        versionOption = infoOption
              (showVersion version)
              (long "version" <> short 'v' <> help "Show version of the program")
 
 config :: Parser Options
 config = Options
-     <$> many (strOption (
-                  long "dotenv"
-                  <> short 'f'
-                  <> metavar "DOTENV"
-                  <> help "File to read for environmental variables" ))
-
-     <*> many (strOption (
-                  long "example"
-                  <> short 'x'
-                  <> metavar "DOTENV_EXAMPLE"
-                  <> help "File to read for needed environmental variables" ))
-
-     <*> switch ( long "overload"
-                  <> short 'o'
-                  <> help "Specify this flag to override existing variables" )
-
-     <*> switch (  long "verbose"
-                  <> help "Specify this flag to print out the variables loaded and other useful insights" )
-
-     <*> switch (  long "dry-run"
-                  <> help "Specify this flag to print out the variables loaded without executing the program" )
-
-     <*> flag True False (  long "no-dups"
-                  <> short 'D'
-                  <> help "Specify this flag to forbid duplicate variables"
-                  )
-
+     <$> dotenvFileOption
+     <*> exampleFileOption
+     <*> overloadOption
+     <*> verboseOption
+     <*> dryRunOption
+     <*> noDuplicatesOption
      <*> argument str (metavar "PROGRAM")
-
      <*> many (argument str (metavar "ARG"))
+
+dotenvFileOption :: Parser [String]
+dotenvFileOption = many
+    ( strOption (
+        long "dotenv"
+        <> short 'f'
+        <> metavar "DOTENV"
+        <> help "File to read for environmental variables" ))
+
+exampleFileOption :: Parser [FilePath]
+exampleFileOption =  many
+    ( strOption (
+        long "example"
+        <> short 'x'
+        <> metavar "DOTENV_EXAMPLE"
+        <> help "File to read for needed environmental variables" ))
+
+overloadOption :: Parser Bool
+overloadOption = switch
+    ( long "overload"
+    <> short 'o'
+    <> help "Specify this flag to override existing variables" )
+
+verboseOption :: Parser Bool
+verboseOption = switch
+    ( long "verbose"
+    <> help "Specify this flag to print out the variables loaded and other useful insights" )
+
+dryRunOption :: Parser Bool
+dryRunOption = switch
+    (  long "dry-run"
+    <> help "Specify this flag to print out the variables loaded without executing the program" )
+
+noDuplicatesOption :: Parser Bool
+noDuplicatesOption = flag True False
+    ( long "no-dups"
+    <> short 'D'
+    <> help "Specify this flag to forbid duplicate variables" )
+
+flagsP :: Parser Flags
+flagsP = Flags
+     <$> dotenvFileOption
+     <*> exampleFileOption
+     <*> overloadOption
+     <*> verboseOption
+     <*> dryRunOption
+     <*> noDuplicatesOption
+
+configFile :: FilePath
+configFile = "dotenv.config"
+
+loadFlagsFromConfig :: IO Flags
+loadFlagsFromConfig = do
+    configExists <- doesFileExist configFile
+    if not configExists then return defaultFlags
+    else do
+        arguments <- words <$> readFile configFile
+        case execParserPure defaultPrefs (mkOpts flagsP) arguments of
+            result@(Failure _) -> do
+                putStrLn "There were errors while parsing the configuration file"
+                handleParseResult result
+            result -> handleParseResult result
